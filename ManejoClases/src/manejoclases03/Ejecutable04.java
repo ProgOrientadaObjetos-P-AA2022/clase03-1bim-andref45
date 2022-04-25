08eb5cae7c60a7acab953029ab343d057f5569ac
André Flores <oaflores7@utpl.edu.ec>
2022-04-25 08:38:17 -0500

    Manejo clases

--- ManejoClases/src/manejoclases03/Ejecutable04.java
+++ ManejoClases/src/manejoclases03/Ejecutable04.java
@@ -10,26 +10,35 @@ package manejoclases03;
  * @author reroes
  */
 public class Ejecutable04 {
-    
+
     public static void main(String[] args) {
-    
+
         Hospital h1 = new Hospital();
         Hospital h2 = new Hospital();
         Hospital h3 = new Hospital();
-        
+
         double valor1 = 10000;
         double valor2 = 20000;
         double valor3 = 30000;
-        
+
         h1.establecerPresupuesto(valor1);
         h2.establecerPresupuesto(valor2);
         h3.establecerPresupuesto(valor3);
-        
-        double suma = h1.obtenerPresupuesto() + h2.obtenerPresupuesto() +
-                h3.obtenerPresupuesto();
-        
+
+        double suma = h1.obtenerPresupuesto() + h2.obtenerPresupuesto()
+                + h3.obtenerPresupuesto();
+
         System.out.printf("La suma de presupuestos es %.2f\n", suma);
         
-        
+
+        h1.establecerNumeroCamas(200);
+        h2.establecerNumeroCamas(50);
+        h3.establecerNumeroCamas(120);
+
+        double suma2 = h1.obtenerNumeroCamas() + h2.obtenerNumeroCamas()
+                + h3.obtenerNumeroCamas();
+
+        System.out.printf("La suma de el número de camas es %.2f\n", suma2);
+
     }
 }
