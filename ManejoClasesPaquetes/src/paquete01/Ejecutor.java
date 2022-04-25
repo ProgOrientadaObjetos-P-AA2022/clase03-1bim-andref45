f40da000073caa97214d6ee2f5203b23668d5583
André Flores <oaflores7@utpl.edu.ec>
2022-04-25 09:33:33 -0500

    Manejo clases y objetos

--- ManejoClasesPaquetes/src/paquete01/Ejecutor.java
+++ ManejoClasesPaquetes/src/paquete01/Ejecutor.java
@@ -5,20 +5,58 @@
  */
 package paquete01;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Ejecutor {
-    
+
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        //agregar valores a los atributos de h1 y h2 por teclado"
+
+        String nombreHospital;
+        int camasHospital;
+        double presupuestoHospital;
+
+        System.out.println("Ingrese el nombre del hospital: ");
+        nombreHospital = entrada.nextLine();
+
+        System.out.println("Ingrese el número de camas del hospital: ");
+        camasHospital = entrada.nextInt();
+
+        System.out.println("Ingrese el presupuesto del hospital: ");
+        presupuestoHospital = entrada.nextDouble();
+
         Hospital h1 = new Hospital();
-        h1.establecerNumeroCamas(10);
-        
+        h1.establecerNombre(nombreHospital);
+        h1.establecerNumeroCamas(camasHospital);
+        h1.establecerPresupuesto(presupuestoHospital);
+
+        System.out.printf("%s - %d - %.2f\n", h1.obtenerNombre(),
+                h1.obtenerNumeroCamas(), h1.obtenerPresupuesto());
+
+        entrada.nextLine();
+        System.out.println("Ingrese el nombre del hospital:(2) ");
+        nombreHospital = entrada.nextLine();
+
+        System.out.println("Ingrese el número de camas del hospital:(2) ");
+        camasHospital = entrada.nextInt();
+
+        System.out.println("Ingrese el presupuesto del hospital:(2) ");
+        presupuestoHospital = entrada.nextDouble();
+
         Hospital h2 = new Hospital();
-        h2.establecerNumeroCamas(10);
-        
-                        
-        System.out.printf("%s \n", "Hola desde el main");
+        h2.establecerNombre(nombreHospital);
+        h2.establecerNumeroCamas(camasHospital);
+        h2.establecerPresupuesto(presupuestoHospital);
+
+        System.out.printf("%s - %d - %.2f\n", h2.obtenerNombre(),
+                h2.obtenerNumeroCamas(), h2.obtenerPresupuesto());
+
     }
 }
